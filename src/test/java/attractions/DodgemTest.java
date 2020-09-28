cc1f16fe34d7fea3d48a749a1b886c031d0668e4
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-28 17:00:50 +0100

    Implements ITicketed on dodgems class and charge half price if visitor is is younger than 12

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,18 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    private Visitor visitor;
+    private Visitor youngVisitor;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor = new Visitor(15, 140, 10);
+        youngVisitor = new Visitor(10, 110, 5);
     }
-
 
     @Test
     public void hasName() {
@@ -28,5 +32,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void getPriceForVisitor(){
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.01);
+    }
+
+    @Test
+    public void getPriceForYoungVisitor(){
+        assertEquals(2.25, dodgems.priceFor(youngVisitor), 0.01);
     }
 }
