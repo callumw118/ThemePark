1b7924127c18d1b9adf577483e0b51945c2434e5
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-28 16:55:36 +0100

    Implements ITicketed to rollercoaster and charges double for taller person

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -11,11 +11,13 @@ public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
     private Visitor visitor;
+    private Visitor tallVisitor;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
         visitor = new Visitor(24, 185, 50);
+        tallVisitor = new Visitor(25, 201, 25);
     }
 
     @Test
@@ -36,5 +38,15 @@ public class RollercoasterTest {
     @Test
     public void canRideRollercoaster(){
         assertTrue(rollerCoaster.isAllowedVisitor(visitor));
+    }
+
+    @Test
+    public void getPriceForVisitor(){
+        assertEquals(8.40, rollerCoaster.priceFor(visitor), 0.01);
+    }
+
+    @Test
+    public void getPriceForTallVisitor(){
+        assertEquals(16.80, rollerCoaster.priceFor(tallVisitor), 0.01);
     }
 }
