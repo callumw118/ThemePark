6d03afcbd444cff1bb39f4970209ab81b4c922de
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-28 16:33:23 +0100

    Implements ISecurity interface and adds methods to classes

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,15 @@
 package attractions;
 
-public class RollerCoaster  extends Attraction {
+import behaviours.ISecurity;
+import people.Visitor;
+
+public class RollerCoaster extends Attraction implements ISecurity {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+    }
+
+    public boolean isAllowedVisitor(Visitor visitor) {
+        return visitor.getHeight() >= 145 && visitor.getAge() >= 12;
     }
 }
--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,19 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlaygroundTest {
     Playground playground;
+    private Visitor visitor;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor = new Visitor(8, 140, 0);
     }
 
     @Test
@@ -27,4 +31,10 @@ public class PlaygroundTest {
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
     }
+
+    @Test
+    public void canVisitPlayground(){
+        assertTrue(playground.isAllowedVisitor(visitor));
+    }
+
 }
